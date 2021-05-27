e2abbe0bb446e4f27c6380a5b3a079d22a7e5bfd
catfi <catfi@DESKTOP-ODBE9BC>
2021-05-27 22:49:28 +0200

    Добавьте файлы проекта.

---
+++ ClasSomov/Pay.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasSomov
+{
+    class Pay
+    {
+        int Day { get; set; }
+        double Oklad { get; set; }
+        double result;
+        public Pay(int days, double sumoOklad)
+        {
+            Day = days;
+            Oklad = sumoOklad;
+        }
+        public void Summa()
+        {
+            result = Oklad / 22 * Day;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Зп = {Math.Round(result,3)}");
+        }
+
+    }
+}
---
+++ ClasSomov/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasSomov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите кол-во отработанных дней: ");
+            int days = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите сумму оклада: ");
+            double oklad = double.Parse(Console.ReadLine());
+            Pay pay = new Pay(days,oklad);
+            pay.Summa();
+            pay.Print();
+            Console.ReadKey();
+        }
+    }
+}
